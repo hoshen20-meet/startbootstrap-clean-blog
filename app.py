2d0d2c36c7af476dd677dcd6c846c3232d3f596d
myname21-meet <myname21@meet.mit.edu>
2020-01-22 06:12:50 -0500

    prgress

--- app.py
+++ app.py
@@ -23,5 +23,42 @@ def contact():
 @app.route('/hello')
 def hello():
     return 'Hello, World'
- 
+
+@app.route('/spesific-book/<int:Book_id>', methods=(['GET','POST']))
+def spesific(Book_id):
+    return render_template('spesific-book.html', Books=query_book_by_id(Book_id))
+
+@app.route('/log_in',methods=['POST','GET'])
+def log_in():
+    if request.method == "GET":
+        return render_template ("log_in.html" )
+    else:
+        name = request.form['uname']
+        password = request.form['psw']
+        if name == 'admin' and password== 'esports123':
+            login_session["admin"]=True
+            return redirect(url_for('store'))
+        else:
+            return render_template ("log_in.html",wrong=True )
+
+    return render_template ("log_in.html" )
+
+
+
+@app.route('/add_book',methods=['POST','GET'])
+def add_books():
+    if request.method == "GET":
+        return render_template ("add_book.html" )
+    else:
+        bookname=request.form['bookname']
+        authorname=request.form['authorname']
+        price=request.form['price']
+        print(request.files)
+        f = request.files['bookpic']
+        f.save("static/img/"+f.filename)
+        add_book(bookname,authorname, price, f.filename)
+        Books = query_all_books()
+        return render_template("store.html",Books=Books)
+
+    
 app.run(debug = True)
